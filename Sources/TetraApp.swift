ee094528452f0bbab2ef913438f923878b5d953a
Vlad Gerasimov <vlad@vlad.studio>
2026-04-08 14:45:15 +0400

    Add config reload method and UI button to trigger it

--- Sources/TetraApp.swift
+++ Sources/TetraApp.swift
@@ -239,6 +239,10 @@ struct MenuBarView: View {
             NSWorkspace.shared.open(path)
         }
 
+        Button("Reload Config") {
+            ConfigManager.shared.reload()
+        }
+
         Toggle("Start at Login", isOn: $launchAtLogin)
             .onChange(of: launchAtLogin) { _, on in
                 if on { LoginItem.enable() } else { LoginItem.disable() }
